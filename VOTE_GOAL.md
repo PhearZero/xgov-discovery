03f182c964017c9f825578d418f8514a83d000bd
Michael Feher <michael.feher@algorand.foundation>
2023-08-23 09:55:53 -0500

    add voting with goal

---
+++ VOTE_GOAL.md
@@ -0,0 +1,57 @@
+# Overview
+
+Voting with `goal` and `algod`
+
+### Links:
+
+- [Current Application][app]
+- [Current Snapshot][snapshot]
+- [Current Questions][questions]
+
+
+### Arguments:
+
+ - **Wallet Address:** Voters wallet
+ - **Voting Round Application ID**: `1158913461` (current voting round)
+ - **Question Options**: `[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]` (index of questions)
+ - **Weighted Vote**: `[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]`
+   - Should equal total weight for the Wallet Address in the [Snapshot][snapshot]
+
+```shell
+./vote.sh -w  <Wallet Address> -a <Voting Round Application Id> -q <Question Options> -v <Weighted Vote>
+```
+
+### Outputs:
+
+A `VoteRound.txn` is produced and can be signed and sent using `goal`.
+
+### Signing:
+
+Signing the VoteRound.txn
+```shell
+goal clerk sign -i VoteRound.txn -o VoteRoundSigned.txn
+```
+
+Sending the Signed Transaction
+```shell
+goal clerk rawsend -f VoteRoundSigned.txn
+```
+
+### Example in sandbox:
+
+Creating the VoteRound.txn
+```shell
+# Create VoteRound.txn
+ ./vote.sh -w GC6CO7Q5MP47VAVICBZ5C5UEGQDG7YXSUTFFOLGXMQYN2AZTJGUIR73EAI -a 1001 -q [0,0] -v [1,1] -i http://192.168.101.245:3000/ipfs
+goal clerk sign -i VoteRound.txn -o VoteRoundSigned.txn
+goal clerk rawsend -f VoteRoundSigned.txn
+# Outputs
+#Raw transaction ID 5VVJEHQFHBTGZPOO7DR5S2VPOOTXGX4NHWXBBQQQNXZQSNISKXPA issued
+#Raw transaction ID 37HM4GERZWSZBMNUKSTEFJU6JE6HKQQTDQU6IVAB3LWEAH2TWHCA issued
+#Transaction 5VVJEHQFHBTGZPOO7DR5S2VPOOTXGX4NHWXBBQQQNXZQSNISKXPA committed in round 824634407160
+#Transaction 37HM4GERZWSZBMNUKSTEFJU6JE6HKQQTDQU6IVAB3LWEAH2TWHCA committed in round 824634407864
+```
+
+[app]:[https://app.dappflow.org/explorer/application/1158913461/transactions]
+[snapshot]:[https://api.voting.algorand.foundation/ipfs/bafkreieh77pgmvfexyxbnbexwu4n5x54kgdfop7lzfo26peyrjcwhn6uii]
+[questions]:[https://api.voting.algorand.foundation/ipfs/bafkreigjiien52ukmfqd5yrjgonrj6ixpr2rm32szps45ztpehk7z4lhli]
---
+++ vote.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+set -e
+
+IPFS_API=https://api.voting.algorand.foundation/ipfs
+OUTPUT=VoteRound.txn
+
+usage() {
+  echo "Usage: $0 [flags]" 1>&2;
+  echo
+  echo "Flag        Description"
+  echo "-w          Wallet Address"
+  echo "-a          Voting Round Application ID"
+  echo "-q          Question Options (example: [0,0,0])"
+  echo "-v          Weighted Vote (example: [1,0,0])"
+  echo "-i          IPFS URL (default: https://api.voting.algorand.foundation/ipfs)"
+  echo "-o          File Output (default: VoteRound.txn)"
+  echo
+  exit 1;
+}
+
+while getopts ":w:a:q:v:i:o:" o; do
+    case "${o}" in
+        w)
+            VOTER_WALLET=${OPTARG}
+            ;;
+        a)
+            APP_ID=${OPTARG}
+            ;;
+        q)
+            VOTE_QUESTION_OPTIONS=${OPTARG}
+            ;;
+        v)
+            VOTE_WEIGHTS=${OPTARG}
+            ;;
+        i)
+            IPFS_API=${OPTARG}
+            ;;
+        o)
+            OUTPUT=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${APP_ID}" ] ||
+   [ -z "${VOTER_WALLET}" ] ||
+   [ -z "${VOTE_QUESTION_OPTIONS}" ] ||
+   [ -z "${VOTE_WEIGHTS}" ]; then
+    usage
+fi
+
+
+# App State
+APP_WALLET=$(goal app info --app-id "$APP_ID" | sed -n '2 p' | awk '{print $3}')
+APP_GLOBAL_STATE=$(goal app read --app-id "$APP_ID" --global --guess-format)
+APP_OUAID=$(echo "$APP_GLOBAL_STATE" | jq ".ouaid | .ui")
+
+# Metadata
+IPFS_METADATA_CID=$(echo "$APP_GLOBAL_STATE" | jq -r ".metadata_ipfs_cid | .tb" )
+IPFS_METADATA=$(curl -s "$IPFS_API"/"$IPFS_METADATA_CID" )
+
+# Snapshot
+IPFS_SNAPSHOT_CID=$(echo "$IPFS_METADATA" | jq -r '.voteGatingSnapshotCid')
+IPFS_SNAPSHOT=$(curl -s "$IPFS_API"/"$IPFS_SNAPSHOT_CID")
+
+# Arguments
+NUM_QUESTIONS=$(echo "$IPFS_METADATA" | jq '.questions | length')
+SIGNATURE=$(echo "$IPFS_SNAPSHOT" | jq ".snapshot | map(select(.address | contains(\"$VOTER_WALLET\"))) | .[] | .signature")
+WEIGHT=$(echo "$IPFS_SNAPSHOT" | jq ".snapshot | map(select(.address | contains(\"$VOTER_WALLET\"))) | .[] | .weight")
+PAYMENT_AMOUNT=$((400*(32+2+NUM_QUESTIONS*1)+2500))
+VOTE_FEE=$((1000 + 16 * 1000))
+BOX_NAME=$(echo -n "V" | base64 -w0)
+
+# Payment Txn
+goal clerk send -a $PAYMENT_AMOUNT -f "$VOTER_WALLET" -t "$APP_WALLET" -o Payment.txn
+
+# Application Txn
+goal app method \
+ --app-id "$APP_ID" \
+ --method="vote(pay,byte[],uint64,uint8[],uint64[],application)void" \
+ --from "$VOTER_WALLET" \
+ --arg "Payment.txn" \
+ --arg "$SIGNATURE" \
+ --arg "$WEIGHT" \
+ --arg "$VOTE_QUESTION_OPTIONS" \
+ --arg "$VOTE_WEIGHTS" \
+ --arg "$APP_OUAID" \
+ --fee $VOTE_FEE \
+ --box "b64:$BOX_NAME" \
+ --box "addr:$VOTER_WALLET" \
+ -o "$OUTPUT"
